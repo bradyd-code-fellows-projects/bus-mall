1e8b6168e56f65a38ce897f5a1c13325632f4a8c
Brady Davenport <brady.davenport@pm.me>
2022-04-18 14:33:50 -0600

    globals, DOMs

---
+++ app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// ********************* GLOBAL VARIABLES *********************
+ 
+let numberOfVotingRounds = 25;
+let productArray = [];
+
+
+// ********************* DOM REFERENCES ***********************
+
+let productImagesContainer = document.getElementById('product-images-container');
+let imgOne = document.getElementById('image-one');
+let imgTwo = document.getElementById('image-two');
+let imgThree = document.getElementById('image-three');
+
+let resultsList = document.getElementById('display-results');
+let resultsBtn = document.getElementById('show-results-btn');
+
+// ********************* CONSTRUCTOR **************************
+
+
+
+// ********** EXECUTBALE CODE - HELPER FUNCTIONS **************
+
+
+
+// ******************* EVENT HANDLERS *************************
+
+
+
+// ****************** EVENT LISTENERS *************************
