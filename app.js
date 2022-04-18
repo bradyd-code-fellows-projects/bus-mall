4fca2351f4666a39cac4d48f78aebf61961c4c95
Brady Davenport <brady.davenport@pm.me>
2022-04-18 16:46:50 -0600

    trying fix problem with png vs jpg extensions

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 // ********************* GLOBAL VARIABLES *********************
 
-let numberOfVotingRounds = 5;
+let numberOfVotingRounds = 10;
 let productArray = [];
 
 
@@ -18,7 +18,8 @@ let resultsBtn = document.getElementById('show-results-btn');
 
 // ********************* CONSTRUCTOR **************************
 
-function Product(name, fileExtension = 'jpg') {
+
+function Product(name, fileExtension) {
   this.productName = name;
   this.img = `img/${name}.${fileExtension}`;
   this.clicks = 0;
@@ -28,25 +29,25 @@ function Product(name, fileExtension = 'jpg') {
 }
 
 new Product('sweep', 'png');
-new Product('bag');
-new Product('banana');
-new Product('bathroom');
-new Product('boots');
-new Product('breakfast');
-new Product('bubblegum');
-new Product('chair');
-new Product('cthulhu');
-new Product('dog-duck');
-new Product('dragon');
-new Product('pen');
-new Product('pet-sweep');
-new Product('scissors');
-new Product('shark');
-new Product('sweep');
-new Product('tauntaun');
-new Product('unicorn');
-new Product('water-can');
-new Product('wine-glass');
+new Product('bag', 'jpg');
+new Product('banana','jpg');
+new Product('bathroom', 'jpg');
+new Product('boots', 'jpg');
+new Product('breakfast', 'jpg');
+new Product('bubblegum', 'jpg');
+new Product('chair', 'jpg');
+new Product('cthulhu', 'jpg');
+new Product('dog-duck', 'jpg');
+new Product('dragon', 'jpg');
+new Product('pen', 'jpg');
+new Product('pet-sweep', 'jpg');
+new Product('scissors', 'jpg');
+new Product('shark', 'jpg');
+new Product('sweep', 'jpg');
+new Product('tauntaun', 'jpg');
+new Product('unicorn', 'jpg');
+new Product('water-can', 'jpg');
+new Product('wine-glass', 'jpg');
 
 
 // ********** EXECUTBALE CODE - HELPER FUNCTIONS **************
@@ -65,7 +66,7 @@ function renderProductImg() {
 
   // make sure each img is different
 
-  while (productOneIndex === productTwoIndex || productOneIndex === productThreeIndex) {
+  while (productOneIndex === productTwoIndex || productOneIndex === productThreeIndex ) {
     productOneIndex = getRandomIndex();
   }
   while (productTwoIndex === productOneIndex || productTwoIndex === productThreeIndex) {
@@ -90,6 +91,8 @@ function renderProductImg() {
 }
 
 renderProductImg();
+
+console.log(productArray[0]);
 
 // ******************* EVENT HANDLERS *************************
 
@@ -121,7 +124,7 @@ function showResultsHandler() {
     for (let i = 0; i < productArray.length; i++) {
       let li = document.createElement('li');
       resultsList.appendChild(li);
-      li.textContent = `${productArray[i].productName} was shown ${productArray[i].views} times and clicked on ${productArray.clicks} times.`;
+      li.textContent = `${productArray[i].productName}: ${productArray[i].views} views, ${productArray[i].clicks} votes.`;
     }
   }
 }
