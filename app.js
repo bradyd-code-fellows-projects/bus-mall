83aad69b785c564c880abc57c8a71795ca3aafc8
Brady Davenport <brady.davenport@pm.me>
2022-04-19 15:07:15 -0600

    brought in chartjs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 // ********************* GLOBAL VARIABLES *********************
 
-let numberOfVotingRounds = 25;
+let numberOfVotingRounds = 4;
 let productArray = [];
 
 
@@ -13,8 +13,13 @@ let imgOne = document.getElementById('image-one');
 let imgTwo = document.getElementById('image-two');
 let imgThree = document.getElementById('image-three');
 
-let resultsList = document.getElementById('display-results');
+// let resultsList = document.getElementById('display-results');
 let resultsBtn = document.getElementById('show-results-btn');
+
+// ******************* CANVAS REFERENCE ***********************
+
+let ctx = document.getElementById('resultsChart');
+
 
 // ********************* CONSTRUCTOR **************************
 
@@ -91,13 +96,68 @@ function renderProductImg() {
 
 renderProductImg();
 
+// ******************* RENDER CHART ***************************
+
+
+function renderResultsChart() {
+  let productNames = [];
+  let productVotes = [];
+  let productViews = [];
+
+  for (let i = 0; i < productArray.length; i++) {
+    const currentProduct = productArray[i];
+    productNames.push(currentProduct.productName);
+    productVotes.push(currentProduct.clicks);
+    productViews.push(currentProduct.views);
+  }
+
+  let resultsChartObj = {
+    type: 'bar',
+    data: {
+      labels: productNames,
+      datasets: [{
+        label: '# of Votes', // # votes and # views
+        data: productVotes,
+        backgroundColor: [
+          'rgba(116, 90, 156, 0.5)'
+        ],
+        borderColor: [
+          'rgba(186, 133, 40, 0.8)'
+        ],
+        borderWidth: 1
+      },
+      {
+        label: '# of Views', // # votes and # views
+        data: productViews, // the actual view or votes
+        backgroundColor: [
+          'rgba(186, 133, 40, 0.8)'
+        ],
+        borderColor: [
+          'brgba(116, 90, 156, 0.5)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  };
+
+
+  new Chart(ctx, resultsChartObj);
+}
+
 
 // ******************* EVENT HANDLERS *************************
 
 function clickHandler(e) {
   let imgClicked = e.target.alt;
 
-  console.log(`${imgClicked} was clicked on`, productArray[0].img);
+  console.log(`${imgClicked} was clicked on`);
 
   for (let i = 0; i < productArray.length; i++) {
     if (imgClicked === productArray[i].productName) {
@@ -109,26 +169,31 @@ function clickHandler(e) {
 
   if (numberOfVotingRounds === 0) {
     productImagesContainer.removeEventListener('click', clickHandler);
+    productImagesContainer.remove();
   }
 
   // three new products now display
 
   renderProductImg();
-
 }
 
 function showResultsHandler() {
   if (numberOfVotingRounds === 0) {
-    for (let i = 0; i < productArray.length; i++) {
-      let li = document.createElement('li');
-      resultsList.appendChild(li);
-      li.textContent = `${productArray[i].productName}: ${productArray[i].views} views, ${productArray[i].clicks} votes.`;
-    }
+    // for (let i = 0; i < productArray.length; i++) {
+    //   let li = document.createElement('li');
+    //   resultsList.appendChild(li);
+    //   li.textContent = `${productArray[i].productName}: ${productArray[i].views} views, ${productArray[i].clicks} votes.`;
+    // }
+    renderResultsChart();
   }
 }
-
 showResultsHandler();
+
+function removeResultsBtn() {
+  resultsBtn.remove();
+}
 // ****************** EVENT LISTENERS *************************
 
 productImagesContainer.addEventListener('click', clickHandler);
 resultsBtn.addEventListener('click', showResultsHandler);
+resultsBtn.addEventListener('click', removeResultsBtn);
